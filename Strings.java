c543fcb68ca478fd7a6f968776057e29c77a9c7f
sai4coding <155803032+sai4coding@users.noreply.github.com>
2024-08-24 10:20:26 +0530

    Add files via upload
    String decleration 
    Strings equal() method

---
+++ Strings.java
@@ -0,0 +1,17 @@
+package com.corejava.strings;
+
+public class Strings {
+    public static void main(String[] args){
+        String st1=new String("naga");
+        String st2=new String("sai");
+        String st3="naga";
+        String st4="sai";
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(st4);
+        System.out.println(st1==st3); //False because == checks address not the data , st1 value stored at heap memory whereas st3 data is stored at String pool
+        System.out.println(st1.equals(st3));
+        
+    }
+}
