543e57088a1fb1379acc3af527b08f91c6965b29
sai4coding <155803032+sai4coding@users.noreply.github.com>
2024-08-26 21:05:38 +0530

    Add files via upload

---
+++ NoOfWords.java
@@ -0,0 +1,14 @@
+package com.corejava.strings;
+import java.util.Arrays;
+
+
+import java.util.Arrays;
+
+public class NoOfWords {
+
+    public static void main(String[] args){
+        String st="java is super";
+        String[] st1=st.split(" ");
+        System.out.println(st1.length);
+    }
+}
