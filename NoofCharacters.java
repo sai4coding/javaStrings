30e03d27c7cdb6bcc88810fdb942a9c415d91173
sai4coding <155803032+sai4coding@users.noreply.github.com>
2024-08-26 21:05:26 +0530

    Add files via upload

---
+++ NoofCharacters.java
@@ -0,0 +1,9 @@
+package com.corejava.strings;
+
+public class NoofCharacters {
+
+    public static void main(String[] args){
+        String st1="java is super";
+        System.out.println(st1.length());
+    }
+}
